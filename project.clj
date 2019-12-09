186fbc200deadc062867c665d505be82c640415f
Grady Simon <grady@gradysimon.com>
2019-12-08 21:47:21 -0800

    Shader running, uniforms not working yet

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [quil "3.1.0"]
-                 [org.clojure/clojurescript "1.10.520"]]
+                 [org.clojure/clojurescript "1.10.520"]
+                 [org.clojure/core.async "0.6.532"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-figwheel "0.5.19"]]
--- src/enso/core.cljs
+++ src/enso/core.cljs
@@ -1,41 +1,62 @@
 (ns enso.core
-  (:require [three]))
+  (:require
+    [clojure.core.async :refer [chan go put! >! <!]]
+    [three]))
 
 (def THREE js/THREE)
 
+(defn fetch-text [url]
+  (go
+    (let [text-chan (chan)
+          fetch-prom (js/fetch url)]
+      (.then fetch-prom (fn [response]
+                          (.then (.text response)
+                                (fn [text]
+                                  (put! text-chan text)))))
+      (<! text-chan))))
+
 (defn init-scene [{:keys [scene] :as state}]
-  (let [geom (THREE.BoxGeometry. 1 1 1)
-        mat (THREE.MeshBasicMaterial. #js {"color" 0x00ff00})
-        cube (THREE.Mesh. geom mat)]
-    (.add scene cube)
-    (assoc-in state [:meshes :cube] cube)))
+  (go
+    (let [geom (THREE.PlaneBufferGeometry. 2 2)
+          uniforms (clj->js
+                    {"u_time" {"type" "f" "value" 0.0}
+                     "u_resolution" {"type" "v2" "value" (THREE.Vector2.)}
+                     "u_mouse" {"type" "v2" "value" (THREE.Vector2.)}
+                     "u_metaball_pos_0" {"type" "v2" "value" (THREE.Vector2. 100 100)}})
+          mat (THREE.ShaderMaterial.
+               (clj->js
+                {"uniforms" uniforms
+                 "vertexShader" (<! (fetch-text "shader/metaball.vert"))
+                 "fragmentShader" (<! (fetch-text "shader/metaball.frag"))}))
+
+          shader-mesh (THREE.Mesh. geom mat)]
+      (.add scene shader-mesh)
+      (-> state
+          (assoc-in [:meshes :shader-mesh] shader-mesh)
+          (assoc-in [:unforms] uniforms)))))
 
 (defn init-camera [{:keys [camera] :as state}]
   (set! camera.position.z 5)
   state)
 
 (defn init []
-  (let [
-        [width height] [js/window.innerWidth js/window.innerHeight]
-        aspect-ratio (/ width height)
-        state {:scene (THREE.Scene.)
-               :camera (THREE.PerspectiveCamera.
-                        75  ; FOV, in degrees
-                        aspect-ratio
-                        0.1 ; Near clipping plane
-                        1000) ; Far clipping plane
-               :renderer (THREE.WebGLRenderer.)
-               :meshes {}}]
-    (.setSize (:renderer state) width height)
-    (js/document.body.appendChild (.-domElement (:renderer state)))
-    (-> state
-        init-scene
-        init-camera)))
+  (go
+    (let [[width height] [js/window.innerWidth js/window.innerHeight]
+          aspect-ratio (/ width height)
+          state {:scene (THREE.Scene.)
+                 :camera (THREE.PerspectiveCamera.
+                          75  ; FOV, in degrees
+                          aspect-ratio
+                          0.1 ; Near clipping plane
+                          1000) ; Far clipping plane
+                 :renderer (THREE.WebGLRenderer.)
+                 :meshes {}}]
+      (.setSize (:renderer state) width height)
+      (js/document.body.appendChild (.-domElement (:renderer state)))
+      (-> (<! (init-scene state))
+          init-camera))))
 
 (defn update-state [state]
-  (let [cube (get-in state [:meshes :cube])]
-    (set! (.-x (.-rotation cube)) (+ 0.01 (.-x (.-rotation cube))))
-    (set! (.-y (.-rotation cube)) (+ 0.01 (.-y (.-rotation cube)))))
   state)
 
 (defn animate [{:keys [scene camera renderer] :as state}]
@@ -46,6 +67,7 @@
 
 (defn main []
   (println "Running enso.core/main")
-  (let [state (init)]
-    (animate state)))
+  (go
+    (let [state (<! (init))]
+      (animate state))))
 
