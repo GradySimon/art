c6294b494de6497299c8b58ad68f6e22bd99cdb7
Grady Simon <grady@gradysimon.com>
2019-12-08 16:39:43 -0800

    Rotating cube and basic state update system

--- src/enso/core.cljs
+++ src/enso/core.cljs
@@ -3,39 +3,49 @@
 
 (def THREE js/THREE)
 
-(defn prep-scene [scene]
+(defn init-scene [{:keys [scene] :as state}]
   (let [geom (THREE.BoxGeometry. 1 1 1)
         mat (THREE.MeshBasicMaterial. #js {"color" 0x00ff00})
         cube (THREE.Mesh. geom mat)]
-    (.add scene cube)))
+    (.add scene cube)
+    (assoc-in state [:meshes :cube] cube)))
 
-(defn prep-camera [camera]
-  (set! camera.position.z 5))
+(defn init-camera [{:keys [camera] :as state}]
+  (set! camera.position.z 5)
+  state)
 
 (defn init []
-  (let [scene (THREE.Scene.)
+  (let [
         [width height] [js/window.innerWidth js/window.innerHeight]
         aspect-ratio (/ width height)
-        camera (THREE.PerspectiveCamera.
-                75  ; FOV, in degrees
-                aspect-ratio
-                0.1 ; Near clipping plane
-                1000) ; Far clipping plane
-        renderer (THREE.WebGLRenderer.)]
-    (.setSize renderer width height)
-    (js/document.body.appendChild renderer.domElement)
-    (prep-scene scene)
-    (prep-camera camera)
-    [scene camera renderer]))
+        state {:scene (THREE.Scene.)
+               :camera (THREE.PerspectiveCamera.
+                        75  ; FOV, in degrees
+                        aspect-ratio
+                        0.1 ; Near clipping plane
+                        1000) ; Far clipping plane
+               :renderer (THREE.WebGLRenderer.)
+               :meshes {}}]
+    (.setSize (:renderer state) width height)
+    (js/document.body.appendChild (.-domElement (:renderer state)))
+    (-> state
+        init-scene
+        init-camera)))
 
+(defn update-state [state]
+  (let [cube (get-in state [:meshes :cube])]
+    (set! (.-x (.-rotation cube)) (+ 0.01 (.-x (.-rotation cube))))
+    (set! (.-y (.-rotation cube)) (+ 0.01 (.-y (.-rotation cube)))))
+  state)
 
-(defn animate [scene camera renderer]
-  (js/requestAnimationFrame #(animate scene camera renderer))
-  (.render renderer scene camera))
+(defn animate [{:keys [scene camera renderer] :as state}]
+  (.render renderer scene camera)
+  (println state)
+  (let [next-state (update-state state)]
+    (js/requestAnimationFrame #(animate next-state))))
 
 (defn main []
-  (println "Hello, world!")
-  (println js/THREE)
-  (let [[scene camera renderer] (init)]
-    (animate scene camera renderer)))
+  (println "Running enso.core/main")
+  (let [state (init)]
+    (animate state)))
 
